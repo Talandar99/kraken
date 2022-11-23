6cc88bd82402c99b6fd7fda96db150eb8e991a6d
Grzegorz <grzegorzc66@gmail.com>
2022-11-23 23:08:02 +0100

    lurking is functional

--- lib/kraken.ex
+++ lib/kraken.ex
@@ -1,23 +1,35 @@
 defmodule Kraken do
   def main([]), do: passedNoArgs()
-  def main(arguments), do: passedArguments(arguments)
+  def main(arguments), do: passed_arguments(arguments)
 
   defp passedNoArgs() do
     IO.puts("""
 
-    ----🐙 KRAKEN 🐙----
-          
-       System helper 
-        from abyss
-
-    --------------------
+    ---------🐙 KRAKEN 🐙----------
         
+            System helper 
+              from abyss
+      
+    -------------------------------
+     lurk  - search for package
+     hunt  - search and install 
+             package
+     call  - git stage . + commit
+     awake - git configuration
+    -------------------------------
+     dive into abyss 
+           - system setup:
+             # nvim + dependencies
+             # git + config
+             # generate ssh key
+    -------------------------------
+
     """)
   end
 
-  defp passedArguments(args) do
+  defp passed_arguments(args) do
     if hd(args) == "lurk" do
-      Lurk.paruLurking(args)
+      Lurk.lurk(args)
     end
   end
 end
--- lib/lurk.ex
+++ lib/lurk.ex
@@ -1,16 +1,21 @@
 defmodule Lurk do
   def lurk(args) do
-    paruLurking(args)
+    IO.puts("Kraken is lurking for #{hd(args)}\n")
+    lurking(args, "pacman")
+    lurking(args, "paru")
   end
 
-  def paruLurking(args) do
-    IO.puts("Kraken is lurking for #{hd(args)}\n")
-    IO.puts("-----{  paru  }-----\n")
-    {resultParu, 0} = System.cmd("paru", ["-Ss" | tl(args)])
-    IO.puts(resultParu)
-    IO.puts("-----{ pacman }-----\n")
-    {resultPacman, 0} = System.cmd("pacman", ["-Ss" | tl(args)])
-    IO.puts("#{resultPacman} ")
-    # IO.inspect(result)
+  def lurking(args, package_manager) do
+    IO.puts("--------------------------------")
+    IO.puts("Lurking with #{package_manager} ")
+    IO.puts("--------------------------------")
+
+    System.cmd(package_manager, ["-Ss" | args])
+    |> convert_output_into_IO()
   end
+
+  def convert_output_into_IO({"", _}),
+    do: IO.puts(IO.ANSI.green() <> "I see only darkness... \n" <> IO.ANSI.reset())
+
+  def convert_output_into_IO({args, _}), do: IO.puts(args <> "\n")
 end
